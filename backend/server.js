d91736e72c59204aa5621962cc0cbfe77ccbee9d
Andy Goh <andygoh@Andys-Mac-mini.local>
2026-01-22 13:14:52 +0800

    dockerized backend with hot reload

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,13 @@
 import express from 'express'
-import dotenv from "dotenv";
-import cors from 'cors'
-import * as controller from './controllers/controller.js'
-
-dotenv.config();
 
 const app = express()
 const port = process.env.EXPRESS_PORT
 
-const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 app.use(express.json())
-app.use(cors(corsOptions))
 
-app.post('/register', async (req, res) => {
-    try {
-        console.log('hit register')
-        await controller.handlePasskeyRegister(req, res)
-    } catch (err) {
-        console.error(err)
-    }
-})
-
-app.post('/login', async (req, res) => {
-    try {
-        console.log('hit login')
-        await controller.handlePasskeyLogin(req, res)
-    } catch (err) {
-        console.error(err)
-    }
+app.post('/api', (req, res) => {
+    console.log('/api received request')
+    res.send('this is a response from /api')
 })
 
 app.listen(port, () => {
